9ab724f344a271935b6a6fd47d59d8c2f6aff927
ghost <ghost@MASTERACE1>
2021-01-16 19:32:38 -0600

    Mi aporte es el "Hola Mundo!" en el archivo AppSistemaBliblioteca.java

---
+++ src/sistemabiblioteca/AppSistemaBiblioteca.java
@@ -0,0 +1,13 @@
+
+package sistemabiblioteca;
+/**
+ *
+ * @author 
+ */
+public class AppSistemaBiblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+    
+}

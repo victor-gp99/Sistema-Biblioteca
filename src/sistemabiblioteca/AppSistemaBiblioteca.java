ea800edf61840c6e2ec8f6663a3e72aeaa8e44c8
ghost <ghost@MASTERACE1>
2021-01-16 19:53:31 -0600

    Mi segundo aporte es el "Prueba actualización!" en el archivo AppSistemaBliblioteca.java

--- src/sistemabiblioteca/AppSistemaBiblioteca.java
+++ src/sistemabiblioteca/AppSistemaBiblioteca.java
@@ -8,6 +8,7 @@ public class AppSistemaBiblioteca {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
+        System.out.println("Prueba actualización...");
     }
     
 }

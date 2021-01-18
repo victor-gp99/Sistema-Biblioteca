a0d4928445a35c3150b888794298c5098a23005b
Diego <Diego@MSI>
2021-01-17 18:48:18 -0600

    Se agrgó mensaje de prueba

--- src/sistemabiblioteca/AppSistemaBiblioteca.java
+++ src/sistemabiblioteca/AppSistemaBiblioteca.java
@@ -9,6 +9,7 @@ public class AppSistemaBiblioteca {
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
         System.out.println("Prueba actualización...");
+        System.out.println("Test - Diego");
     }
     
 }

21ccdf3026421e8beef9fe24dcb0c3e88ad4aa3e
joefi <joefi@OMEN-PP>
2021-01-25 02:13:40 -0600

    Select libro para enviar a mostrar libro

--- src/sistemabiblioteca/LibreriaUI.java
+++ src/sistemabiblioteca/LibreriaUI.java
@@ -206,7 +206,7 @@ public class LibreriaUI extends javax.swing.JFrame {
     }//GEN-LAST:event_labelCarritoMouseClicked
 
     private void libreriaTableMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_libreriaTableMouseClicked
-        new MostrarLibro();
+        new MostrarLibro(showBook(String.valueOf(libreriaTable.getValueAt(libreriaTable.getSelectedRow(), 0)))).setVisible(true);
     }//GEN-LAST:event_libreriaTableMouseClicked
 
     public static void main(String args[]) {
@@ -309,9 +309,32 @@ public class LibreriaUI extends javax.swing.JFrame {
     }else
        JOptionPane.showMessageDialog(this, "Selecciona un libro para agregar al carrito...");     
     }   
-    public String [] showBook(){
-        String book [] = {"Hola","Mundo"};
-        
+    public String [] showBook(String libroID){
+        String book [] = new String[8];
+        book[0] = libroID;
+        try {
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("SELECT l.titulo, concat(a.nombre,' ',a.apellido1,' ',a.apellido2),l.descripcion, l.tipo, l.isbn, l.anio_publicacion, "
+                    + "p.precio, e.precio FROM libro AS l LEFT JOIN libro_papel AS p ON  l.id = p.id LEFT JOIN libro_ebook AS e ON l.id = e.id inner JOIN autor_escribe_libro as al"
+                    + " ON l.id = al.id_libro join autor as a ON al.id_autor = a.id WHERE l.id ="+libroID);
+            while(rs.next()){
+                book[1] = rs.getString(1);
+                book[2] = rs.getString(2);
+                book[3] = rs.getString(3);
+                book[4] = rs.getString(4);
+                book[5] = rs.getString(5);
+                book[6] = rs.getString(6);
+                if(book[4].equals("Papel"))
+                    book[7] = rs.getString(7);
+                else
+                    book[7] = rs.getString(8);
+            }
+            st.close();
+            rs.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(this, "Error en la base de datos", "MySQL", JOptionPane.ERROR_MESSAGE);
+            Logger.getLogger(LibreriaUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return book;
     }
 }

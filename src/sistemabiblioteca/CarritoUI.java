781f10caa228bb4770f7f97dc2911cb3cc053bfd
ghost <ghost@MASTERACE1>
2021-01-25 20:46:37 -0600

    Oculte id de la tabla

--- src/sistemabiblioteca/CarritoUI.java
+++ src/sistemabiblioteca/CarritoUI.java
@@ -116,7 +116,7 @@ public class CarritoUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+       
     }//GEN-LAST:event_jButton1ActionPerformed
 
     public static void main(String args[]) {
